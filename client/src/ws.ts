0381267b1cb1d30adf62f44cf5582422613da8f5
mangtrungzos <sangv906@gmail.com>
2025-04-24 12:43:37 +0700

    Change

--- client/src/ws.ts
+++ client/src/ws.ts
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
 
-const WS =  process.env.REACT_APP_WS_URL || "http://localhost:8080"; 
-// export const WS = "http://13.210.68.104";
+export const WS = "https://realtime-chat-app.fly.dev"; 
+// export const WS =  process.env.REACT_APP_WS_URL || "https://realtime-chat-app.fly.dev"; 
 export const ws = socketIOClient(WS);
